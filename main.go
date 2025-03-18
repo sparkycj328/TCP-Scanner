71af0a1fec8e1b7ff717b22a57e97126ff0e1897
Carlos <sparkycj328@gmail.com>
2025-03-18 23:58:07 +0100

    Add initial structure to support worker groups

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
-	"net"
+	"sync"
 )
 
-func main() {
-	if _, err := net.Dial("tcp", "scanme.nmap.org:80"); err == nil {
-		fmt.Println("Connection successful!")
-	} else {
-		fmt.Println(err.Error())
+// worker is a function to process the work it receives
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
 	}
+}
+
+func main() {
+	//var (
+	//	address    = "scanme.nmap.org:%s"
+	//	successMsg = "%s open\n"
+	//)
+	var wg sync.WaitGroup
+
+	ports := make(chan int, 1)
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
+		ports <- i
+	}
+	wg.Wait()
+	close(ports)
 }
